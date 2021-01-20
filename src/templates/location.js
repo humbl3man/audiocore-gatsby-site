672b1c9e825dbbdbbf9f6dede07f9a50bfd14493
Konstantin M <humbl3dev@gmail.com>
2021-01-20 14:06:51 -0800

    provide description to location detail page

--- src/templates/location.js
+++ src/templates/location.js
@@ -1,21 +1,37 @@
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
 import React from "react";
 import { FaChevronRight } from "react-icons/fa";
 
-const LocationDetail = ({ pageContext }) => {
-  const { locationName } = pageContext;
+const LocationDetail = ({ data }) => {
+  const { sanityLocations: location } = data;
   return (
     <div className="page">
       <div className="container text-sm mb-6 grid auto-cols-max gap-2 grid-flow-col items-center">
-        <Link to="/locations">Locations</Link>
-        <FaChevronRight className="text-gray-400" />
-        <span>{locationName}</span>
+        <Link
+          to="/locations"
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          Locations
+        </Link>
+        /<span className="text-gray-600">{location.name}</span>
       </div>
       <div className="container">
-        <h1 className="page-heading">{locationName}</h1>
+        <h1 className="page-heading">{location.name}</h1>
+        <p>{location.description}</p>
       </div>
     </div>
   );
 };
+
+export const pageQuery = graphql`
+  query($locationName: String!) {
+    sanityLocations(name: { eq: $locationName }) {
+      name
+      description
+    }
+  }
+`;
 
 export default LocationDetail;
