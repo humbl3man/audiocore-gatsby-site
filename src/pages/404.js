cad0700a3ac8200400516be63131838578fb978b
Konstantin M <humbl3dev@gmail.com>
2021-01-20 14:06:02 -0800

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,10 @@ import SEO from "../components/seo";
 const NotFoundPage = () => (
   <div className="container">
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 className="page-heading text-center">Page Not Found</h1>
+    <p className="text-center">
+      Sorry, we can't find the page you're looking for.
+    </p>
   </div>
 );
 
