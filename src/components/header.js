436853b5f155b85a2dcdb1e767923e556db41ddc
Konstantin M <humbl3dev@gmail.com>
2021-01-21 18:13:39 -0800

    add mobile nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,18 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
-import React from "react";
-import { FaHeadphones } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaHeadphones, FaBars, FaTimes } from "react-icons/fa";
 
 const Header = ({ siteTitle, isHomePage }) => {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  function closeMobileNav() {
+    setMobileNavOpen(false);
+  }
+  function openMobileNav() {
+    setMobileNavOpen(true);
+  }
+
   return (
     <header
       className={` text-black ${
@@ -12,14 +21,22 @@ const Header = ({ siteTitle, isHomePage }) => {
           : "mb-10 bg-purple-600 text-white"
       }`}
     >
-      <div className="container py-6 md:flex md:items-center md:justify-between">
+      <div className="container py-6 flex items-center justify-between">
         <h1 className="my-0 font-bold uppercase tracking-wide">
           <Link to="/" className="inline-flex items-center">
             <FaHeadphones className="mr-2 text-3xl" />
             <span className="text-xl">{siteTitle}</span>
           </Link>
         </h1>
-        <nav className="mt-6 grid gap-6 auto-cols-max grid-flow-col md:mt-0">
+        <button
+          type="button"
+          className="block text-2xl md:hidden"
+          onClick={openMobileNav}
+        >
+          <FaBars />
+        </button>
+        {/* Desktop Nav */}
+        <nav className="hidden md:grid md:gap-6 md:auto-cols-max md:grid-flow-col md:mt-0">
           <Link
             className="opacity-90 hover:opacity-100"
             activeStyle={{ opacity: 1 }}
@@ -42,6 +59,51 @@ const Header = ({ siteTitle, isHomePage }) => {
           >
             Locations
           </Link>
+        </nav>
+        {/* Mobile Nav */}
+        <nav
+          className={`${
+            !mobileNavOpen ? "hidden " : ""
+          }fixed inset-0 w-full h-full bg-purple-600 z-10 md:hidden`}
+        >
+          <button
+            type="button"
+            onClick={closeMobileNav}
+            aria-label="Close"
+            className="mt-4 px-4 mb-6 w-full flex items-center justify-end text-2xl"
+          >
+            <FaTimes />
+          </button>
+          <div className="px-4 grid grid-flow-row gap-5">
+            <Link
+              onClick={closeMobileNav}
+              className="opacity-90 hover:opacity-100"
+              activeStyle={{ opacity: 1 }}
+              to="/"
+              className="text-xl"
+            >
+              Home
+            </Link>
+            <Link
+              onClick={closeMobileNav}
+              className="opacity-90 hover:opacity-100"
+              activeStyle={{ opacity: 1 }}
+              to="/about"
+              className="text-xl"
+            >
+              About
+            </Link>
+            <Link
+              onClick={closeMobileNav}
+              className="opacity-90 hover:opacity-100"
+              activeStyle={{ opacity: 1 }}
+              partiallyActive={true}
+              to="/locations/"
+              className="text-xl"
+            >
+              Locations
+            </Link>
+          </div>
         </nav>
       </div>
     </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,9 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className="hero bg-purple-800 py-40 flex items-center pl-8 text-white h-screen">
       <div className="container">
-        <h1 className="mt-0 mb-3 text-8xl">AudioC0re</h1>
+        <h1 className="mt-0 mb-3 text-4xl md:text-6xl lg:text-8xl">
+          AudioC0re
+        </h1>
         <p className="text-2xl opacity-90">Redefine your shopping experience</p>
         <Link
           className="py-2 px-4 opacity-90 rounded bg-white text-black shadow-sm mt-8 inline-flex items-center hover:opacity-80 transition-opacity"
